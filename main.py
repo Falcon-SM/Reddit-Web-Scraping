358306c6afdd4292342cc7e5c142b35b940527d6
FalconTFG <idsdabomb@gmail.com>
2021-02-11 16:44:32 +0000

    Added a check to make sure that you didn't enter an invalid subreddit!

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
-import lxml
 import replit
 import colorama as cr
+
 replit.clear()
 
 cr.init(autoreset=True)
@@ -33,54 +33,70 @@ response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.content, 'lxml')
 
 Title = soup.title
-
-print (url)
-print('')
-print('')
-print (f"{cr.Fore.GREEN}there are no ads on this version of reddit!")
-print ('')
-print ('')
-print ('')
-print ('')
-
+try:
+  for item in soup.select('.Post'):
+    print(item.select('_2zJbrt0pYl6tbRsmVW0peX _3goHjUTM8-J0xINP6EoZkZ _3t7aUZU2b2KWwDQkfT2eHl _10BQ7pjWbeYP63SAPNS8Ts HNozj_dKjQZ59ZsfEegz8 ')[0].get_text())
+  print('')
+  print(f"{cr.Fore.RED}it appears that you entered an invalid subreddit!")
+  print('')
+except:
+  print("")
 
 
-for item in soup.select('.Post'):
-      if item.select('._1rZYMD_4xY3gRcSS3p8ODO')[0].get_text() != '•' and "k" in (item.select('._1rZYMD_4xY3gRcSS3p8ODO')[0].get_text())and 'k' in (item.select('.FHCV02u6Cp2zYL0fhQPsO')[0].get_text()):
-            print(f"{cr.Fore.YELLOW}Post Title: " + f"{cr.Fore.WHITE}" + item.select('._eYtD2XCVieq6emjKBH3m')[0].get_text())
-            print('')
-            print('Votes: ' + item.select('._1rZYMD_4xY3gRcSS3p8ODO')[0].get_text())
-            print('')
-            print(item.select('.FHCV02u6Cp2zYL0fhQPsO')[0].get_text())
-            print('')
-            
-            try:
-              print(f"{cr.Fore.YELLOW}Flair: " + f"{cr.Fore.WHITE}" + item.select('._2X6EB3ZhEeXCh1eIVA64XM')[0].get_text())
-            except Exception as e:
-              e = "No Flair"
-              print(e)
-            
+  print (url)
+  print('')
+  print('')
+  print (f"{cr.Fore.GREEN}there are no ads on this version of reddit!")
+  print ('')
+  print ('')
+  print ('')
+  print ('')
 
 
 
-            try:
-              print (" "+ + item.select('._3Oa0THmZ3f5iZXAQ0hBJ0k ')[0])	
-            except Exception as EEE:
-              EEE = ""
-              print(EEE)
+    
+
+
+  for item in soup.select('.Post'):
+        if item.select('._1rZYMD_4xY3gRcSS3p8ODO')[0].get_text() != '•' and "k" in (item.select('._1rZYMD_4xY3gRcSS3p8ODO')[0].get_text())and 'k' in (item.select('.FHCV02u6Cp2zYL0fhQPsO')[0].get_text()):
+              print(f"{cr.Fore.YELLOW}Post Title: " + f"{cr.Fore.WHITE}" + item.select('._eYtD2XCVieq6emjKBH3m')[0].get_text())
+              print('')
+              print('Votes: ' + item.select('._1rZYMD_4xY3gRcSS3p8ODO')[0].get_text())
+              print('')
+              print(item.select('.FHCV02u6Cp2zYL0fhQPsO')[0].get_text())
+              print('')
+              
+              try:
+                print(f"{cr.Fore.YELLOW}Flair: " + f"{cr.Fore.WHITE}" + item.select('._2X6EB3ZhEeXCh1eIVA64XM')[0].get_text())
+              except Exception as e:
+                e = "No Flair"
+                print(e)
+              
+
+
+
+              try:
+                print (" "+ + item.select('._3Oa0THmZ3f5iZXAQ0hBJ0k ')[0])	
+              except Exception as EEE:
+                EEE = ""
+                print(EEE)
 
 
 
 
-            print(f"{cr.Fore.YELLOW}Original Post: " + f"{cr.Fore.WHITE}reddit.com" + item.select('._2INHSNB8V5eaWp4P0rY_mE a[href]')[0]['href'])	
+              print(f"{cr.Fore.YELLOW}Original Post: " + f"{cr.Fore.WHITE}reddit.com" + item.select('._2INHSNB8V5eaWp4P0rY_mE a[href]')[0]['href'])	
 
-            print('')
+              print('')
 
-            print('------------------------')
+              print('------------------------')
+        else:
+          print('')
+          print(f"{cr.Fore.GREEN}there WAS an ad here but I conveniently removed it for you!")
+          print('')
+          print('------------------------')
 
 
 
-print("nothing Here? You might have entered an invalid subreddit or sort method!")
 
 
   
